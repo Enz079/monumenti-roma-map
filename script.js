debeece5a0ae94ba8e49a9895f8b9fc44f3c07ba
Enzo Z <zheng.enzo@itismeucci.com>
2026-04-21 21:29:08 +0200

    aggiunta form

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 aggiungiMonumenti(map);
 
 mostraStatisticheMonumenti();
+
+const form = document.getElementById('monumento-form');
+
+form.addEventListener('submit', function(e) {
+    
+    e.preventDefault(); // Evita il refresh della pagina
+    
+    const nome = document.getElementById('nome-monumento').value;
+    const lat = parseFloat(document.getElementById('lat-monumento').value);
+    const lng = parseFloat(document.getElementById('lng-monumento').value);
+    
+    const nuovoMonumento = {
+        nome: nome,
+        lat: lat,
+        lng: lng
+    };
+    
+    aggiungiMonumentoArray(nuovoMonumento);
+    
+    //Aggiunge il marker sulla mappa
+    const marker = L.marker([lat, lng]).addTo(map);
+    marker.bindPopup(`<b>${nome}</b>`);
+    
+    form.reset();
+    
+    alert(`Monumento "${nome}" aggiunto con successo!`);
+});
+
+
+const btnStatistiche = document.getElementById('btn-statistiche');
+btnStatistiche.addEventListener('click', function() {
+    mostraStatisticheMonumenti();
+});
